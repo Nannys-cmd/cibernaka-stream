27dd33105a9aeace35a2927548b21731a2403482
Daniela Bastias <bastiasdany.84@gmail.com>
2025-12-23 19:18:34 -0300

    Creando la página

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import './Header.css'
+
+function Header() {
+  return (
+    <header className="header">
+      <h1 className="retro-title">CiberNaka Stream</h1>
+      <p className="header-subtitle">
+        Retro gaming · Streams · Descargas
+      </p>
+    </header>
+  )
+}
+
+export default Header
